ded80baa3f35488e0861c2e87a48191f2cc65dc8
Sherin Varughese <Sherin.V@ibm.com>
2019-11-04 13:40:36 -0500

    CICD pipeline changes - update to semver versioning (#36)
    * CICD versioning update
    
    * revert changes
    
    * debug 1
    
    * revert changes for test

--- travis-env.sh
+++ travis-env.sh
@@ -4,19 +4,8 @@
 # Use, duplication or disclosure restricted by GSA ADP Schedule 
 # Contract with IBM Corp. 
 
-# Release Tag
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    RELEASE_TAG=latest
-else
-    RELEASE_TAG="${TRAVIS_BRANCH#release-}-latest"
-fi
-if [ "$TRAVIS_TAG" != "" ]; then
-    RELEASE_TAG="${TRAVIS_TAG#v}"
-fi
-export RELEASE_TAG="$RELEASE_TAG"
-
-# Release Tag
+# Echo vars
+echo Travis event details:
 echo TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE
 echo TRAVIS_BRANCH=$TRAVIS_BRANCH
 echo TRAVIS_TAG=$TRAVIS_TAG
-echo RELEASE_TAG="$RELEASE_TAG"
