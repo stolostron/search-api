a1e9f78ee21d138d742545a081e1df4f8c57e9e9
Jorge Padilla <jpadilla@us.ibm.com>
2020-02-11 22:54:37 -0500

    debug

--- build/build.sh
+++ build/build.sh
@@ -6,5 +6,11 @@ echo "Running build/build.sh"
 export DOCKER_IMAGE_AND_TAG=${1}
 make lint
 make build-prod
+
+echo ">> make prune"
 make prune
+
+echo ">> make docker/build"
 make docker/build
+
+echo "DONE Running build/build.sh"
