53dd273b3cdfe54d37f337b5292ac21e76f4475b
Jorge Padilla <jpadilla@us.ibm.com>
2020-02-11 23:02:24 -0500

    debug

--- build/build.sh
+++ build/build.sh
@@ -2,11 +2,16 @@
 set -e
 
 echo "Running build/build.sh"
-
+echo "Current dir:"
+pwd
 export DOCKER_IMAGE_AND_TAG=${1}
 make lint
+echo "Current dir:"
+pwd
 make build-prod
 
+echo "Current dir:"
+pwd
 echo ">> make prune"
 make prune
 
