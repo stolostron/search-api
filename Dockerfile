5bba036c20b19bde8078dfb3cfec2bf29936a58f
Jorge Padilla <jpadilla@us.ibm.com>
2020-02-12 00:19:15 -0500

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -36,10 +36,8 @@ ENV BABEL_DISABLE_CACHE=1 \
     USER_UID=1001 \
     VCS_REF="$VCS_REF"
 
-RUN mkdir -p /opt/app-root/search-api /licenses
-ADD license.txt /licenses
+RUN mkdir -p /opt/app-root/search-api
 WORKDIR /opt/app-root/search-api
-
 COPY . /opt/app-root/search-api
 
 EXPOSE 4010
--- build/build.sh
+++ build/build.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "Running build/build.sh"
-echo "Current dir:"
-pwd
+echo "> Running build/build.sh"
+
 export DOCKER_IMAGE_AND_TAG=${1}
 make lint
-echo "Current dir:"
-pwd
-ls /home/travis/build/open-cluster-management/search-api/build
+
 make build-prod
 
-echo "Current dir:"
-pwd
-ls /home/travis/build/open-cluster-management/search-api/build
-echo ">> make prune"
 make prune
 
 echo ">> make docker/build"
