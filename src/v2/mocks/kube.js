8bfc80ec1838c95fa214ad1368fbd0550dc5ed74
Jorge Padilla <jpadilla@us.ibm.com>
2020-04-14 10:35:15 -0400

    Copyright

--- src/v2/mocks/kube.js
+++ src/v2/mocks/kube.js
@@ -6,6 +6,7 @@
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
  ****************************************************************************** */
+// Copyright (c) 2020 Red Hat, Inc.
 
 /**
  * NOTE: For brevity the response objects for user access infomation have been GREATLY reduced.
