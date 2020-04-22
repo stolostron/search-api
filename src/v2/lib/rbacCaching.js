b5f5d06fd962ccd4c692aa7ae80a227a7dd34719
Zack Layne <zlayne@redhat.com>
2020-04-22 17:22:46 -0400

    Fix user access logic to handle null values

--- src/v2/lib/rbacCaching.js
+++ src/v2/lib/rbacCaching.js
@@ -158,8 +158,11 @@ async function getUserAccess(kubeToken, namespace) {
   const rules = (isOpenshift ? res.status.rules : res.status.resourceRules) || [];
 
   // Check if user can get all resources in namespace.
-  if (rules.find(r =>
-    ((r.verbs.includes('*') || r.verbs.includes('get')) && r.apiGroups.includes('*') && r.resources.includes('*')))) {
+  if (rules.find(r => (
+    r.verbs && (r.verbs.includes('*') || r.verbs.includes('get'))
+    && (r.apiGroups && r.apiGroups.includes('*'))
+    && (r.resources && r.resources.includes('*'))
+  ))) {
     return [`${namespace}_*_*`];
   }
 
@@ -169,7 +172,7 @@ async function getUserAccess(kubeToken, namespace) {
       // RBAC string is defined as "namespace_apigroup_kind"
       const resources = [];
       const ns = (namespace === '' || namespace === undefined) ? 'null_' : `${namespace}_`;
-      const apiGroup = (rule.apiGroups[0] === '' || rule.apiGroups[0] === undefined) ? 'null_' : `${rule.apiGroups[0]}_`;
+      const apiGroup = _.get(rule, 'apiGroups[0]', '') === '' ? 'null_' : `${rule.apiGroups[0]}_`;
       // Filter sub-resources, those contain '/'
       rule.resources.filter(r => r.indexOf('/') === -1).forEach((resource) => {
         resources.push(`'${ns + apiGroup + resource}'`);
