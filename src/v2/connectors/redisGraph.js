774e62be115dbb3880465c65c099b23800bbcc91
Zack Layne <zlayne@redhat.com>
2020-07-09 17:03:08 -0400

    Update src/v2/connectors/redisGraph.js
    Co-authored-by: Jorge Padilla <jpadilla@redhat.com>

--- src/v2/connectors/redisGraph.js
+++ src/v2/connectors/redisGraph.js
@@ -224,6 +224,9 @@ export default class RedisGraphConnector {
     }
     const whereClauseRbac = aliases.map((alias) => {
       if (nsValues.length > 0) {
+        // When user is allowed to see all resources in a given namespace:
+        //  - For hub resources, we use the `namespace` field.
+        //  - For managed cluster resources, use the `_clusterNamespace`, which is the namespace where the cluster is mapped on the hub.
         return `${alias}._rbac IN allowedResources OR ((exists(${alias}._hubClusterResource) AND (${alias}.namespace IN allowedNS)) OR (exists(${alias}._clusterNamespace) AND ${alias}._clusterNamespace IN allowedNS))`;
       }
       return `${alias}._rbac IN allowedResources`;
