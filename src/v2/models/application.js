996bd0e112be7014bc55d2300d2148cc7fffd6cd
Kevin Cormier <kcormier@redhat.com>
2020-12-07 10:02:56 -0500

    Filter '-local' subscriptions

--- src/v2/models/application.js
+++ src/v2/models/application.js
@@ -139,11 +139,26 @@ export default class AppModel {
     this.checkSearchServiceAvailable();
 
     const subs = await this.searchConnector.runSubscriptionsQuery();
+    const localSuffix = '-local';
+
+    // Filter subscriptions ending with '-local' unless there is no corresponding subscription without '-local'
+    // These subscriptions are added automatically when a PlacementRule subscribes the local cluster, and they
+    // are not really part of the application definition
+    const resolvedSubs = (await subs).filter((sub) => {
+      const subName = sub['sub.name'];
+
+      if (!subName.endsWith(localSuffix)) {
+        return true;
+      }
+      const subNameWithoutLocal = subName.substr(0, subName.length - localSuffix.length);
+      return subs.find((subNonLocal) => subNonLocal['sub.namespace'] === sub['sub.namespace']
+        && subNonLocal['sub.name'] === subNameWithoutLocal) === undefined;
+    });
+
     if (name != null && namespace != null) {
-      const resolvedSubs = await subs;
       return resolvedSubs.filter((sub) => (sub['sub.name'] === name && sub['sub.namespace'] === namespace));
     }
-    return subs;
+    return resolvedSubs;
   }
 
   /*
