d779341c979e083ec23cd0d79d3d8090d0e1f2a9
dislbenn <lavontae.bennett@gmail.com>
2020-12-10 17:39:25 -0500

    updated api.test.js

---
+++ src/v2/lib/api.test.js
@@ -0,0 +1,14 @@
+/** *****************************************************************************
+ * Licensed Materials - Property of Red Hat, Inc.
+ * Copyright (c) 2020 Red Hat, Inc.
+ ****************************************************************************** */
+
+describe('Search API: ', () => {
+  beforeEach(() => {
+    console.log('This is the before.');
+  });
+
+  it('should create resources', () => {
+    console.log('im creating a resource');
+  });
+});
