0e9d45be997629a81df82d415175269d0d109d7f
Sherin Varughese <shvarugh@redhat.com>
2021-03-02 23:37:54 -0500

    Add RH copyright

--- src/v2/connectors/kube.test.js
+++ src/v2/connectors/kube.test.js
@@ -6,6 +6,7 @@
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
  ****************************************************************************** */
+// Copyright (c) 2021 Red Hat, Inc.
 // Copyright Contributors to the Open Cluster Management project
 
 import KubeConnector from './kube';
