5505ed78ac24eca5f83de95e8a4bcd69b9f6edd4
Jorge Padilla <jpadilla@redhat.com>
2021-04-27 18:00:00 -0400

    Update searchServiceStatus.js

--- src/v2/models/searchServiceStatus.js
+++ src/v2/models/searchServiceStatus.js
@@ -5,9 +5,7 @@ import logger from '../lib/logger';
 export async function getOperatorStatus(kubeConnector) {
   const namespace = process.env.POD_NAMESPACE || 'open-cluster-management';
   const operator = await kubeConnector.get(`/apis/search.open-cluster-management.io/v1alpha1/namespaces/${namespace}/searchoperators/searchoperator`);
-  const deployRedisgraph = operator && operator.status && operator.status.deployredisgraph;
-
-  return deployRedisgraph;
+  return operator && operator.status && operator.status.deployredisgraph;
 }
 
 export async function checkSearchServiceStatus(searchConnector, kubeConnector) {
