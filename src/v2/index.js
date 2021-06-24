35c885e72afe4dccbee9ee92a492f7931174a2af
anxhela <acoba@redhat.com>
2021-06-24 18:00:57 -0400

    lint

--- src/v2/index.js
+++ src/v2/index.js
@@ -88,20 +88,20 @@ const requestLogger = isProd
   })
   : morgan('dev');
 
-if (process.env.NODE_ENV === "development"){
-graphQLServer.use('*', helmet({
-  frameguard: false,
-  noSniff: false,
-  xssFilter: false,
-  contentSecurityPolicy: false, // adding this for graphQL api
-}), noCache(), requestLogger, cookieParser());
-
+if (process.env.NODE_ENV === 'development') {
+  graphQLServer.use('*', helmet({
+    frameguard: false,
+    noSniff: false,
+    xssFilter: false,
+    contentSecurityPolicy: false, // adding this for graphQL api
+  }), noCache(), requestLogger, cookieParser());
 } else {
   graphQLServer.use('*', helmet({
-  frameguard: false,
-  noSniff: false,
-  xssFilter: false,
-}), noCache(), requestLogger, cookieParser());}
+    frameguard: false,
+    noSniff: false,
+    xssFilter: false,
+  }), noCache(), requestLogger, cookieParser());
+}
 
 graphQLServer.get('/livenessProbe', (req, res) => {
   res.send(`Testing livenessProbe --> ${new Date().toLocaleString()}`);
