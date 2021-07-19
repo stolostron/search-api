99c58bd58b81afdc4b6e72a6d5e14f8c1ee7d4dd
Zack Layne <zlayne@redhat.com>
2021-07-19 12:45:00 -0400

    seearch-api needs to handle image property as a list
    Signed-off-by: Zack Layne <zlayne@redhat.com>

--- src/v2/connectors/redisGraph.js
+++ src/v2/connectors/redisGraph.js
@@ -23,7 +23,7 @@ import { isRequired } from '../lib/utils';
 import pollRbacCache, { getUserRbacFilter } from '../lib/rbacCaching';
 
 export function getPropertiesWithList() {
-  return ['label', 'role', 'port', 'container', 'category', 'rules', 'addon'];
+  return ['label', 'role', 'port', 'container', 'category', 'rules', 'addon', 'image'];
 }
 
 // Is there a more efficient way?
