e084139ae9a228031bafd8ec8d33e6aff48fad5e
Jorge Padilla <jpadilla@redhat.com>
2021-08-10 21:46:54 -0400

    Add lint target to Makefile.prow

--- build/install-dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2021 Red Hat, Inc.
-# Copyright Contributors to the Open Cluster Management project
-set -e
-
-echo "> Running build/install-dependencies.sh"
-make install
-exit 0
--- build/run-e2e-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2021 Red Hat, Inc.
-# Copyright Contributors to the Open Cluster Management project
-set -e
-
-echo "> Running build/run-e2e-tests.sh"
-export DOCKER_IMAGE_AND_TAG=${1}
-# make docker/run
