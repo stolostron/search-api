2d05d612551d422cd2f5ef654722fb615bf1b375
Jorge Padilla <jpadilla@redhat.com>
2022-06-28 23:46:26 -0400

    Alternative
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- src/v2/models/search.js
+++ src/v2/models/search.js
@@ -17,7 +17,8 @@ import { checkSearchServiceStatus } from './searchServiceStatus';
 // Remove single and double quotes because these can be used to inject malicious
 // code in the RedisGraph query. (SQL injection).
 function sanitizeString(s) {
-  return s.replace(/['"()]/g, '');
+  // return s.replace(/['"()]/g, '');           // Less risk of breaking functionality.
+  return s.replace(/[^a-zA-Z0-9\.\-_=/]/g, ''); // Less risk of injection, but could be missing valid chars.
 }
 
 // Sanitize all inputs to prevent "sql injection" attacks.
